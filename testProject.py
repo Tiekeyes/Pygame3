3a9a64ea37d3eb215c16165f3e703bd4a4917be9
Tiekeyes <sakakiharataiki@gmail.com>
2026-06-21 00:17:47 -0700

    Changed Location of Assets

--- testProject.py
+++ testProject.py
@@ -1,25 +1,29 @@
 import pygame as game
 import sys
-game.init()
 
+class Game:
+    def __init__(self):
+        game.init()
 
+        #set display
+        game.display.set_caption("Platformer Game")
+        self.screen = game.display.set_mode((640, 480))
+        self.clock = game.time.Clock()
 
-screen = game.display.set_mode((640, 640))
-clock = game.time.Clock()
+        self.img = game.image.load("data\images\clouds\cloud_1.png")
 
-#set display name
-game.display.set_caption("Platformer Game")
+    def run(self):
+        #main game loop
+        while True:
+            
+            self.screen.blit(self.img, (100, 200))
+            
+            for event in game.event.get():
+                if(event.type == game.QUIT):
+                    game.quit()
+                    sys.exit()
 
-running = True
-while running:
-    for event in game.event.get():
-        if(event.type == game.QUIT):
-            game.quit()
-            sys.exit()
-            running = False
+            game.display.update()
+            self.clock.tick(60) #runs at 60 fps, dynamic sleep function
 
-    game.display.update()
-    clock.tick(60) #runs at 60 fps, dynamic sleep function
-
-game.quit()
-
+Game().run()
