c2c700c643b1c8269c82b0c2c16fd99bfd4e0932
Tiekeyes <sakakiharataiki@gmail.com>
2026-06-21 00:36:31 -0700

    Added basic collision

--- testProject.py
+++ testProject.py
@@ -15,6 +15,9 @@ class Game:
         self.img_pos = [160, 260]
         self.movement = [False, False]
 
+        self.collision_area = game.Rect(50, 50, 300, 50)
+
+
     def run(self):
         #main game loop
         while True:
@@ -24,6 +27,12 @@ class Game:
             self.img_pos[1] += self.movement[1] - self.movement[0]
             self.screen.blit(self.img, self.img_pos)
             
+            img_r = game.Rect(self.img_pos[0], self.img_pos[1], self.img.get_width(), self.img.get_height())
+            if(img_r.colliderect(self.collision_area)):
+                game.draw.rect(self.screen,(0, 100, 255), self.collision_area)
+            else:
+                 game.draw.rect(self.screen,(0, 50, 255), self.collision_area)
+
             for event in game.event.get():
 
                 if(event.type == game.QUIT):
@@ -31,11 +40,14 @@ class Game:
                     sys.exit()
 
                 #keyboard inputs
+
+                #when pressed
                 if(event.type == game.KEYDOWN):
                     if(event.key == game.K_UP):
                         self.movement[0] = True
                     if(event.key == game.K_DOWN):
                         self.movement[1] = True
+                #when released
                 if(event.type == game.KEYUP):
                     if(event.key == game.K_UP):
                         self.movement[0] = False
