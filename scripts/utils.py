54e1fc5bbe9111a19bc60acda928204b5fca9445
Tiekeyes <sakakiharataiki@gmail.com>
2026-06-22 12:37:00 -0700

    Added player animations

--- scripts/utils.py
+++ scripts/utils.py
@@ -34,8 +34,10 @@ class Animation:
             self.frame = (self.frame + 1) % (self.img_duration * len(self.images))
         else:
             self.frame = min(self.frame + 1, self.img_duration * len(self.images) - 1)
+            if self.frame >= self.img_duration * len(self.images) - 1:
+                self.done = True
 
 
     def img(self):
-        return self.images[int(self.frame) / self.img_duration]
+        return self.images[int(self.frame // self.img_duration)]
     
