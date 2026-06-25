544d84f9f520bb42333a518fc3b27f667e305014
Tiekeyes <sakakiharataiki@gmail.com>
2026-06-24 22:26:33 -0700

    Finished level editor

--- scripts/editor.py
+++ scripts/editor.py
@@ -30,6 +30,12 @@ class Editor:
         
         #create tilemap
         self.tilemap = Tilemap(self, tile_size = 16)
+        
+        try:
+            #self.tilemap.load('data/maps/' + input("Map name:") + '.json')
+            self.tilemap.load("data/maps/my_map.json")
+        except FileNotFoundError:
+            print("Map File does not Exist")
 
         #instantiate camera position
         self.scroll = [0, 0]
@@ -37,10 +43,11 @@ class Editor:
         self.tile_list = list(self.assets)
         self.tile_group = 0
         self.tile_variant = 0
+
         self.clicking = False
         self.right_clicking = False
         self.shift = False
-
+        self.ongrid = True
     def run(self):
         #main game loop
         while True:
@@ -55,25 +62,30 @@ class Editor:
 
             #display the current tile selected
             current_tile_img = self.assets[self.tile_list[self.tile_group]][self.tile_variant].copy() 
-            current_tile_img.set_alpha(100)
+            current_tile_img.set_alpha(150)
 
             #returns the pixel coordiantes of the mouse in respect to the WINDOW
             mouse_pos = pygame.mouse.get_pos()
             mouse_pos = (mouse_pos[0] / RENDER_SCALE, mouse_pos[1] / RENDER_SCALE)
             tile_pos = ((int(mouse_pos[0] + self.scroll[0]) // self.tilemap.tile_size), 
                         (int(mouse_pos[1] + self.scroll[1]) // self.tilemap.tile_size))
-
-            self.display.blit(current_tile_img, (tile_pos[0] * self.tilemap.tile_size - self.scroll[0], tile_pos[1] * self.tilemap.tile_size - self.scroll[1]))
-            
+            if(self.ongrid):
+                self.display.blit(current_tile_img, (tile_pos[0] * self.tilemap.tile_size - self.scroll[0], tile_pos[1] * self.tilemap.tile_size - self.scroll[1]))
+            elif(not self.ongrid):
+                self.display.blit(current_tile_img, (mouse_pos[0] - (self.tilemap.tile_size / 2), mouse_pos[1] - (self.tilemap.tile_size / 2)))
             #place tiles
-            if(self.clicking):
+            if(self.clicking and self.ongrid):
                 self.tilemap.tilemap[str(tile_pos[0]) + ";" + str(tile_pos[1])] = {"type": self.tile_list[self.tile_group], "variant": self.tile_variant, "pos": tile_pos}
             #delete tiles
             if(self.right_clicking):
                 tile_loc = str(tile_pos[0]) + ";" + str(tile_pos[1])
                 if tile_loc in self.tilemap.tilemap:
                     del self.tilemap.tilemap[tile_loc]
-
+                for tile in self.tilemap.offgrid_tiles.copy():
+                    tile_img = self.assets[tile["type"]][tile["variant"]]
+                    tile_hitbox = pygame.Rect(tile["pos"][0] - self.scroll[0], tile["pos"][1] - self.scroll[1], tile_img.get_width(), tile_img.get_height())
+                    if tile_hitbox.collidepoint(mouse_pos):
+                        self.tilemap.offgrid_tiles.remove(tile)
 
             for event in pygame.event.get():
 
@@ -86,6 +98,10 @@ class Editor:
                 if(event.type == pygame.MOUSEBUTTONDOWN):
                     if(event.button == 1):
                         self.clicking = True
+                        if(not self.ongrid):
+                            self.tilemap.offgrid_tiles.append({"type": self.tile_list[self.tile_group], 
+                                                               "variant": self.tile_variant, 
+                                                               "pos": (mouse_pos[0] - (self.tilemap.tile_size/2) + self.scroll[0], mouse_pos[1] - (self.tilemap.tile_size/2) + self.scroll[1])})
                     if(event.button == 3):
                         self.right_clicking = True
 
@@ -111,26 +127,36 @@ class Editor:
 
                 #when a key is pressed
                 if(event.type == pygame.KEYDOWN):
-                    if(event.key == pygame.K_LEFT):
+                    if(event.key == pygame.K_a):
                         self.movement[0] = True
-                    if(event.key == pygame.K_RIGHT):
+                    if(event.key == pygame.K_d):
                         self.movement[1] = True
-                    if(event.key == pygame.K_UP):
+                    if(event.key == pygame.K_w):
                         self.movement[2] = True
-                    if(event.key == pygame.K_DOWN):
+                    if(event.key == pygame.K_s):
                         self.movement[3] = True
+                    if(event.key == pygame.K_q):
+                        self.ongrid = not self.ongrid
+                    if(event.key == pygame.K_c):
+                        self.tilemap.tilemap.clear()
+                        self.tilemap.offgrid_tiles.clear()
+                    if(event.key == pygame.K_e):
+                        #self.tilemap.save("data/maps/" + input("Map name:") + ".json")
+                        self.tilemap.save("data/maps/my_map.json")
+                    if(event.key == pygame.K_r):
+                        self.tilemap.autotile()
                     if(event.key == pygame.K_LSHIFT):
                         self.shift = True
 
                 #when released
                 if(event.type == pygame.KEYUP):
-                    if(event.key == pygame.K_LEFT):
+                    if(event.key == pygame.K_a):
                         self.movement[0] = False
-                    if(event.key == pygame.K_RIGHT):
+                    if(event.key == pygame.K_d):
                         self.movement[1] = False
-                    if(event.key == pygame.K_UP):
+                    if(event.key == pygame.K_w):
                         self.movement[2] = False
-                    if(event.key == pygame.K_DOWN):
+                    if(event.key == pygame.K_s):
                         self.movement[3] = False
                     if(event.key == pygame.K_LSHIFT):
                         self.shift = False
--- testProject.py
+++ testProject.py
@@ -45,6 +45,7 @@ class Game:
         self.player = Player(self, (50, 50), (8, 15))
         #create tilemap
         self.tilemap = Tilemap(self, tile_size = 16)
+        self.tilemap.load("data/maps/my_map.json")
 
     def run(self):
         #main game loop
